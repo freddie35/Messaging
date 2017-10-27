f6437987539dd8f834d8d75b3d43bff00a88b96d
Ismael <iuc14leamsi@gmail.com>
2017-10-27 10:37:19 -0700

    Messaging

---
+++ css/message.js
@@ -0,0 +1,12 @@
+
+
+document.addEventListener('click',function(){
+	var input = document.getElementById('field_data').value;
+	console.log(input);
+	localStorage.setItem("MAMA",input);
+});
+	document.addEventListener('post',function(){
+		input = document.addEventListener('display_data').innerHTML;
+});
+
+
